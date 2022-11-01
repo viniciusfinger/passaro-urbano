86ac2b49cab2968264924b68906ff6c49bd99e50
ViniciusFinger <finger-pessoal@cnx-viniciusfinger.lan>
2022-11-01 08:24:35 -0300

    injecting ofertaService correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertaService } from './services/oferta.service';
 
 @Component({
   selector: 'xyz-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [OfertaService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'passaro-urbano';
+
+  constructor(private ofertaService : OfertaService){}
+
+  ngOnInit(){
+    console.log(this.ofertaService.getOfertas());
+  }
 }
