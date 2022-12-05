b72e126ca2a33ba552d5c1da7f0d2357734843b9
ViniciusFinger <finger-pessoal@cnx-viniciusfinger.lan>
2022-12-05 20:52:40 -0300

    creating pages diversao and restaurantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
     BrowserModule,
