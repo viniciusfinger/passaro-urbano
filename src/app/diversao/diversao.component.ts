3e458021a52131f67bc566021eb81962cb21b203
ViniciusFinger <finger-pessoal@cnx-viniciusfinger.lan>
2022-12-05 21:56:13 -0300

    dynamizing diversao

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertaService } from '../services/oferta.service';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertaService]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas : Array<Oferta>;
+
+  constructor(private ofertaService : OfertaService) { }
 
   ngOnInit() {
+    this.ofertaService.getOfertasPorCategoria('diversao')
+      .then((ofertas : Array<Oferta>) => this.ofertas = ofertas)
+      .catch((err : any) => console.log(err));
   }
-
 }
